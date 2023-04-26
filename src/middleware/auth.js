c5532f44b771a9404d1eb658a022d6946abf1600
joshsolace <joshuakovic@gmail.com>
2023-04-26 12:16:23 +0100

    chores

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,51 +4,6 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID:"831203606301-i5enj0qnj5fouo6ge67qplovn9d2ucgh.apps.googleusercontent.com",
-//       clientSecret:"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",
-//       callbackURL: "http://localhost:5163/auth/google/callback",
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       const currentUser = await pool.query(
-//         "SELECT * FROM users WHERE googleId = $1",
-//         [profile.id]
-//       );
-//       if (currentUser.rows.length <= 0) {
-//         const currentUser= rows[0];
-//         const token = jwt.sign({ id: currentUser.id }, process.env.JWT_SECRET, {
-//           expiresIn: "1h",
-//         });
-//         console.log(`user is ${currentUser.id}`);
-//         console.log(`token is ${token}`);
-//         console.log(currentUser);
-//         done(null, currentUser, token);
-//       }else{
-//         const user = await pool.query(
-//           "INSERT INTO users (googleId, username, photo, email) VALUES ($1, $2, $3, $4) RETURNING *",
-//           [
-//             profile.id,
-//             profile.displayName,
-//             profile._json.picture,
-//             profile.emails[0].value,
-//           ]
-//           );
-//           const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, {
-//             expiresIn: "1h",
-//           });
-//           console.log(`user is ${user.rows[0].id}`);
-//           console.log(`token is ${token}`);
-//           console.log(user.rows[0]);
-//           done(null, user, token);
-//         }
-      
-//         done(null, currentUser);
-//     }
-
-//   )
-// );
 
 passport.use(
   new GoogleStrategy(
